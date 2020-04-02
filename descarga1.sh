75e04310bee5b703f9cadf82433a23d65a6852a2
unknown <croelanjr@gmail.com>
2020-04-01 19:21:32 -0500

    version01

---
+++ descarga1.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#wget --referer http://support.amd.com/  https://drivers.amd.com/drivers/linux/19.50/amdgpu-pro-19.50-967956-rhel-8.1.tar.xz
+
+#wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+
+wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-8-linux-x86_64.tar.gz
+
---
+++ stop-grin31.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+/usr/bin/killall -w -s 2 grin31
+/root/amdgpu-pro-fans/amdgpu-pro-fans.sh -s 50
